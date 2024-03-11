e1c9532efdad4e427fac49fcbfb8aef9ccba7762
jktr3025 <jktr3025@naver.com>
2024-03-11 20:02:55 +0900

    portfolio day6

--- script/myindex.js
+++ script/myindex.js
@@ -4,15 +4,29 @@ $(function(){
             var typingIdx = 0; //타이핑 될 글자의 인덱스
             var liIdx = 0; // li의 인덱스 
             var liLength = $(".motto >ul >li").length;  // li 갯수
+            var typingTxt = ""
+            $(".typing ul li").removeClass("on");
+            $(".typing ul li").text("")
             
             //타이핑 될 텍스트 가져오기
             var typingTxt = $(".motto > ul >li").eq(liIdx).text();
             console.log(typingTxt);
             typingTxt = typingTxt.split(""); // 한글자씩 글자 자르기
             console.log(typingTxt);
+
             if(typingBool == false){
                 typingBool = true;
                 var time = setInterval( typing, 100) //반복 동작
+            } else{
+                typingBool = false;
+                typingIdx = 0;
+                liIdx = 0 ;
+                liLength = $(".motto >ul >li").length;
+                typingTxt = $(".motto > ul >li").eq(liIdx).text();
+                typingTxt = typingTxt.split("");
+                typingTxt = ""
+                $(".typing ul li").removeClass("on");
+                $(".typing ul li").text(""); 
             }//if
 
             function typing(){
@@ -21,8 +35,7 @@ $(function(){
                 if(typingIdx < typingTxt.length){
                     $(".typing ul li").eq(liIdx).append(typingTxt[typingIdx]);
                 typingIdx++;
-                }
-                else{
+                }else{
                     if(liIdx < liLength -1){
                         liIdx++;   //다음 줄로 이동하기 위한 인덱스 번호 증가
                         //다음 문장을 타이핑 하기 위한 셋팅
@@ -34,19 +47,21 @@ $(function(){
                         clearInterval(time);
                         setTimeout(function(){
                             time = setInterval( typing, 100)
-                        }, 1000)
-                    }//작은 if
-                }//큰if
+                        }, 1000);
+                    } else if(liIdx ==liLength-1){
+                        clearInterval(time);
+                    }//liIdx < liLength -1
+                }//typingIdx < typingTxt.length
             }//typing
 
 
-
+            
     if($(window).width() > 390){
         $('#fullpage').fullpage({
                 autoScrolling: true,
                 scrollHorizontally: true,
                 keyboardScrolling:true,
-                ScrollingSpeed:4000,
+                ScrollingSpeed:5000,
                 // navigationTooltips:["","", "","","","",""] ,
                 menu: ".menu",
                 slidesNavigation:true  , //슬라이드의 버튼 여부
@@ -55,32 +70,47 @@ $(function(){
                 afterLoad: function (index) {
                     var current = index.index;
                     console.log("current :", current)
-                    if (current == 1 ) {
+                    if (current == 1 || current == 2) {
             //타이핑 효과
-            var typingBool = false;
-            var typingIdx = 0; //타이핑 될 글자의 인덱스
-            var liIdx = 0; // li의 인덱스 
-            var liLength = $(".motto >ul >li").length;  // li 갯수
-             $(".typing ul li").text("");
-             $(".typing ul li").removeClass("on");
+
+            $(".typing ul li").removeClass("on");
+            $(".typing ul li").text("")
+            
+            typingBool = false;
+            typingIdx = 0; //타이핑 될 글자의 인덱스
+             liIdx = 0; // li의 인덱스 
+             typingTxt = ""
+             liLength = $(".motto >ul >li").length;  // li 갯수
+            
             //타이핑 될 텍스트 가져오기
-            var typingTxt = $(".motto > ul >li").eq(liIdx).text();
+             typingTxt = $(".motto > ul >li").eq(liIdx).text();
             console.log(typingTxt);
             typingTxt = typingTxt.split(""); // 한글자씩 글자 자르기
             console.log(typingTxt);
+
             if(typingBool == false){
                 typingBool = true;
-                 time = setInterval( typing, 100) //반복 동작
+                var time = setInterval( typing, 100) //반복 동작
+            } else{
+                typingBool = false;
+                typingIdx = 0;
+                liIdx = 0 ;
+                liLength = $(".motto >ul >li").length;
+                typingTxt = $(".motto > ul >li").eq(liIdx).text();
+                typingTxt = typingTxt.split("");
+                typingTxt = ""
+                $(".typing ul li").removeClass("on");
+                $(".typing ul li").text("");
             }//if
+
             function typing(){
                 $(".typing ul li").removeClass("on");
                 $(".typing ul li").eq(liIdx).addClass("on");
                 if(typingIdx < typingTxt.length){
                     $(".typing ul li").eq(liIdx).append(typingTxt[typingIdx]);
                 typingIdx++;
-                }
-                else{
-                    if(liIdx < liLength -1){
+                }else{
+                    if(liIdx < liLength - 1){
                         liIdx++;   //다음 줄로 이동하기 위한 인덱스 번호 증가
                         //다음 문장을 타이핑 하기 위한 셋팅
                         typingBool = false;
@@ -90,17 +120,13 @@ $(function(){
                         //다음문장 타이핑 전 1초 쉬기
                         clearInterval(time);
                         setTimeout(function(){
-                        var    time = setInterval( typing, 100)
-                        }, 1000)
-                    }//작은 if
-                }//큰if
-            }//typing
-                    }else{
-                        $(".typing ul li").text("");
-                    } //if
-                } //afterLoad
-            },
-
+                            time = setInterval( typing, 100)
+                        }, 1000);
+                    } else if(liIdx ==liLength-1){
+                        clearInterval(time);
+                    }//liIdx < liLength -1
+                }//typingIdx < typingTxt.length
+            } //typing
 
 
             //메뉴 클릭 시 바 이동
@@ -128,11 +154,11 @@ $(function(){
 
                         //coding hover
                         $(".c_finish a").hover(function(){
-                            $(".c_finish1 img").stop().animate({top:"-485%"},4000)
-                            $(".c_finish2 img").stop().animate({top:"-213%"},2000)
+                            $(".c_finish1 img").stop().animate({top:"-486.5%"},4000)
+                            $(".c_finish2 img").stop().animate({top:"-214%"},2000)
                             $(".c_finish3 img").stop().animate({top:"-17%"},500)
-                            $(".c_finish4 img").stop().animate({top:"-374%"},4000)
-                            $(".c_finish5 img").stop().animate({top:"-114%"},1000)
+                            $(".c_finish4 img").stop().animate({top:"-478%"},4000)
+                            $(".c_finish5 img").stop().animate({top:"-105%"},1000)
                             $(".c_finish6 img").stop().animate({top:"-86%"},1000)
                             $(".c_finish7 img").stop().animate({top:"-374%"},4000)
                         }, function(){
@@ -144,19 +170,9 @@ $(function(){
                             $(".c_finish6 img").stop().animate({top:"0"},1000)
                             $(".c_finish7 img").stop().animate({top:"0"},4000)
                         })
+                    }}
 
-
-            )//fullpage
-
-
-
-
-
-
-
-
-
-
+                })//fullpage
         }//if390보다 클 때
 
 })//jquery
--- script/myintro.js
+++ script/myintro.js
@@ -2,6 +2,17 @@ $(function(){
     setInterval(function(){
 
     $(".world").css({"background-position":"-=10"});
+    
 }, 50)//setInterval
+
+
+$({val:0}).animate({val:100}, {duration:6000, step : function(){
+    $(".progress1").text(Math.floor(this.val));
+    $(".progress1").css({color:"#fff"})
+}, 
+complete:function(){
+    $(".progress1").text(Math.floor(this.val));
+}
+})//progress1
 
 })//jquery  
